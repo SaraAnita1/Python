23b621707f41233e185f7e0a40437bf7db8a855c
SaraAnita1 <sara.anita.desa@gmail.com>
2022-10-28 19:56:52 -0300

    Aprendendo Váriaveis e Constantes

--- scr.py
+++ scr.py
@@ -1,4 +1,53 @@
+#Aprendendo Hello World 
+#Aula 1
+
 print ("Gostosa, porem cansada!")
 print ("Hello World")
 print ("¬¬")
 print ("que seja sucesso")
+
+#Aprendendo Váriaveis e constantes em python
+    #Aula 2
+
+age = 25
+name = 'Sara'
+print(f'Meu nome é {name} e eu tenho {age} ano(s) de idade')
+
+age, name = (25, 'Sara')
+print(f'meu nome é {name} e eu tenho {age} anos de idade')
+
+age = 24
+name = 'Vitor'
+igual = 'Amor da minha vida'
+print (f'O nome dele é {name} ele tem {age} de idade, conhecido tambem como {igual}.')
+
+age = 25
+name = 'Sara'
+igual = 'Amor da vida do Vitor'
+print (f'O meu nome é {name} eu tenho {age} de idade, conhecida tambem como {igual}.')
+
+    #constantes
+    #ela nasce e permanece com o mesmo valor.
+
+#não existe palavra para determinar que é uma constante = para fazer isso você deve utilizar o nome da váriavel todo em maiusculo.
+#assim como combinado por convenção.
+        #Boas Práticas
+# 1 - Padrão de nomes deve ser em snake case
+    #todos os espaços vazios deve ser colocados _
+# 2 - Escolher nomes Sugestivos.
+    #exemplo: sempre dizer de fato o que a variavel está fazendo naquele código.
+# 3 - Nome de Constante sempre em maiúsculo.
+
+
+name = 'Sara'
+age = 25
+
+name, age = 'Vitor', 24
+
+print (name, age)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print (BRAZILIAN_STATES)
+
